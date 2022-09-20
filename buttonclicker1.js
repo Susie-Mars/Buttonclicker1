9e064ec35cf605bff79bd1f7fb849d903451fbac
Susie-Mars <marsnarsbars@gmail.com>
2022-09-19 20:45:20 -0600

    buttonclicker

---
+++ buttonclicker1.js
@@ -0,0 +1,9 @@
+function clicked(element) {
+    element.innerText= "Logout";
+    }
+    function hide(element) {
+        element.remove()
+    }
+    function myFunction() {
+        alert("Ninja was liked");
+    }
